058e8e76dd337f223837309179f23c834c545e40
nikunj.work@gmail.com <nikunj@nikunjs-Mac-mini.local>
2024-01-28 01:06:14 +0530

    change placehoder into register screen

--- components/RegisterForm.jsx
+++ components/RegisterForm.jsx
@@ -69,17 +69,17 @@ export default function RegisterForm() {
           <input
             onChange={(e) => setName(e.target.value)}
             type="text"
-            placeholder="Full Name"
+            placeholder="Enter Your Full Name"
           />
           <input
             onChange={(e) => setEmail(e.target.value)}
             type="text"
-            placeholder="Email"
+            placeholder="Enter Your Email"
           />
           <input
             onChange={(e) => setPassword(e.target.value)}
             type="password"
-            placeholder="Password"
+            placeholder="Enter Your Password"
           />
           <button className="bg-green-600 text-white font-bold cursor-pointer px-6 py-2">
             Register
